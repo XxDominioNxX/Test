b6972477b41274ac094d30e78dc5e3bf7b3281a6
XxDominioNxX <92040109+XxDominioNxX@users.noreply.github.com>
2024-06-15 14:39:32 +1000

    Add files via upload

---
+++ Task_1.cs
@@ -0,0 +1,40 @@
+﻿//---------------Use-----------------//
+
+var t = new Triangle(3, 4, 5);
+var s = new Circle(3);
+
+Console.WriteLine(GetArea(t));
+Console.WriteLine(GetArea(s));
+
+
+
+
+
+//--------------Library----------------//
+
+double GetArea<T>(T figure) where T : FigureBase => figure.CalcArea();
+
+class Triangle(double a, double b, double c) : FigureBase
+{
+    private double p = (a + b + c) / 2;
+    public override double CalcArea() => Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+    public bool IsRectangular
+    {
+        get 
+        { 
+            return a * a + b * b == c * c; 
+        }
+    }
+}
+
+
+class Circle(double radius) : FigureBase
+{
+    public override double CalcArea() => Math.PI * radius * radius;
+}
+
+
+abstract class FigureBase
+{
+    public abstract double CalcArea();
+}
